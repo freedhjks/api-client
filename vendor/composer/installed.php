c038fb92ad6607cf75818e7a481cb675cf61e5ea
freedhjks <211208873+freedhjks@users.noreply.github.com>
2025-05-16 22:29:58 +0000

    Pending changes exported from your codespace

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dbip/api-client',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '165be358d5681e6093f57b30ad6de574c25b1a92',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dbip/api-client' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '165be358d5681e6093f57b30ad6de574c25b1a92',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
